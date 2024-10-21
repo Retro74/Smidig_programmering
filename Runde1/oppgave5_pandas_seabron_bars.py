215a8c1f48280f161fe5949c963c2085e730ef3c
Retro74 <roger.mikalsen@gmail.com>
2024-10-21 10:35:49 +0200

    Updates

--- Runde1/oppgave5_pandas_seabron_bars.py
+++ Runde1/oppgave5_pandas_seabron_bars.py
@@ -4,12 +4,10 @@ import matplotlib.pyplot as plt
 
 filnavn = "Runde1/Innbyggertall_Byer.csv"
 innbyggere_byer_df = pandas.read_csv(filnavn)
-
-
 # Eksempeldata: "tips" datasett fra Seaborn
 #tips = sns.load_dataset("tips")
-
 # Lag en enkel Seaborn graf (f.eks. scatter plot)
+
 sns.barplot(x="By", y="Innbyggertall", data=innbyggere_byer_df)
 #print(type(tips))
 # Vise grafen
