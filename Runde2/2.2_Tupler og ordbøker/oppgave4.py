c145340e1aeb6f54a0667cef6ae72006ecd376e2
Retro74 <roger.mikalsen@gmail.com>
2024-11-08 13:26:12 +0100

    lala

--- Runde2/2.2_Tupler og ordbøker/oppgave4.py
+++ Runde2/2.2_Tupler og ordbøker/oppgave4.py
@@ -1,19 +1,28 @@
+import matplotlib.pyplot as plt 
 from pathlib import Path
 fileName = 'standpunktskarakterer_R1.csv' 
 filePath = Path(__file__).resolve().parent
 fullPath =f"{filePath}\{fileName}"
 fileData =[]
-frekvenser = [0]*6
-karakterer = [1,2,3,4,5,6]
+##
+kar_frekvens = {1:0,2:0,3:0,4:0,5:0,6:0}
 with open(fullPath, encoding="utf8") as fil:
     overskrift = fil.readline().strip().split(",")
     for linje in fil:
         tmp_linje = linje.strip().split(",")
         tmp_linje[-1] = int(tmp_linje[-1])
-        frekvenser[tmp_linje[-1]-1]+=1
+        kar_frekvens[tmp_linje[-1]]+=1
         fileData.append(tmp_linje)
 
-print(frekvenser)
-print(sum(frekvenser))
-#print(fileData)
+##Lager en ordbok med relative frekvenser
+kar_rel_frekvens = {}
+antall_elever = sum(kar_frekvens.values())
+for karakter, antall in kar_frekvens.items():
+    kar_rel_frekvens[karakter] = antall / antall_elever
 
+print(kar_frekvens)
+print(kar_rel_frekvens)
+#print(sum(frekvenser))
+#plt.bar(kar_frekvens.keys(), kar_frekvens.values())
+plt.pie(kar_frekvens.values(), labels=kar_frekvens.keys())
+plt.show()
