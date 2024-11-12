9f28b399032512666ab733e6ed303f097edf0da2
Retro74 <roger.mikalsen@gmail.com>
2024-11-12 14:58:00 +0100

    update

--- Runde2/2.2_Tupler og ordbøker/oppgave10.py
+++ Runde2/2.2_Tupler og ordbøker/oppgave10.py
@@ -3,23 +3,26 @@ import re
 from itertools import chain
 fileName = 'Miljøutfordringer er et av vår tids.txt' 
 filePath = Path(__file__).resolve().parent
-fullPath =f"{filePath}\{fileName}"
-fileData =[]
-with open(fullPath, encoding="utf8") as fil:
-    for linje in fil:
-        tmp_linje = linje.lower().strip().split(" ") 
-        for i in range(len(tmp_linje)):
-            tmp_linje[i] = re.sub(r'[^a-zA-Z0-9æøåÆØÅ\s]', '', tmp_linje[i])
-        fileData.append(tmp_linje)
-fileData = list(chain.from_iterable(fileData))
-print(len(fileData))
+fullPath =f"{filePath}\\{fileName}"
 
+with open(fullPath, encoding="utf8") as fil:
+    #Leser inn alt og splitter det i ord med små bokstaver uten linjeskift 
+    data_fil = fil.read().lower().strip().split(" ")
+
+#Gjennomløper alle ord og tar bort spesialtegn
+for i in range(len(data_fil)):
+    data_fil[i] = re.sub(r'[^a-zA-Z0-9æøåÆØÅ\s]', '', data_fil[i])
+
+#print(len(data_fil))
+
+##Oppretter ordliste for å telle antall forekomster
 ordliste = {}
-for ord in fileData:
+for ord in data_fil:
     if ord in ordliste:
         ordliste[ord]+=1
     else:
         ordliste[ord]=1
 
+#Sorterer orlisten etter antall forekomster i synkende rekkefølge
 ordliste = dict(sorted(ordliste.items(),reverse=True, key=lambda item: item[1]))
 print(ordliste)
